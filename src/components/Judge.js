c709d3c9e427a545b99c175e5dc2f09d03d8f93b
Sumato Ken <bermime1@gmail.com>
2022-07-23 20:34:30 +0100

    Board design finished

---
+++ src/components/Judge.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function () {
+  return (
+    <svg
+      width="16"
+      height="24"
+      viewBox="0 0 16 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8 24V14.5M8 14.5H5V9H8H11V14.5H8Z"
+        stroke="#FF0000"
+        stroke-width="4"
+      />
+      <ellipse cx="8" cy="9" rx="8" ry="9" fill="#D82626" />
+    </svg>
+  );
+}
---
+++ src/components/Pin.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Pin() {
+  return (
+    <svg
+      width="26"
+      height="33"
+      viewBox="0 0 26 33"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <ellipse cx="12.5" cy="23" rx="8.5" ry="10" fill="#C93131" />
+      <ellipse cx="13" cy="13.5" rx="13" ry="13.5" fill="#FF0000" />
+    </svg>
+  );
+}
